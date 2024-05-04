8f90d8b685908ec949c5b1a42ca97aa5233d6433
juancruzgauna <juancruzgauna97@gmail.com>
2024-05-04 11:59:40 -0300

    EJERCITACION PARCIAL 1

--- EJERCICIOS TIPO PARCIAL 1/SECRETARIA DE TRANSPORTES/secretariaTransportes.cpp
+++ EJERCICIOS TIPO PARCIAL 1/SECRETARIA DE TRANSPORTES/secretariaTransportes.cpp
@@ -17,57 +17,36 @@ void cargarCadena(char *pal, int tam)
     fflush(stdin); ///vuelve a limpiar el buffer para eliminar los caracteres sobrantes
 }
 
-class Fecha
-{
-private:
-    int dia;
-    int mes;
-    int anio;
-public:
-    Fecha(int d=1, int m=1, int a=1900)
-    {
-        dia = d;
-        mes = m;
-        anio = a;
-    }
-    void setDia(int d)
-    {
-        if(d > 0 && d < 32)
-        {
+class Fecha{
+    private:
+        int dia;
+        int mes;
+        int anio;
+    public:
+        Fecha(int d=1, int m=1, int a=1900){
             dia=d;
+            mes=m;
+            anio=a;
         }
-    }
-    void setMes(int m)
-    {
-        mes = m;
-    }
-    void setAnio(int a)
-    {
-        anio = a;
-    }
-    void setearFecha(int d, int m, int a)
-    {
-        dia = d;
-        mes = m;
-        anio = a;
-    }
-    int getDia()
-    {
-        return dia;
-    }
-    int getMes()
-    {
-        return mes;
-    }
-    int getAnio()
-    {
-        return anio;
-    }
-    void Cargar();
-    void Mostrar()
-    {
-        cout<<dia<<"/"<<mes<<"/"<<anio;
-    }
+        void setDia(int d){
+            if(d>0 && d<32){
+                dia=d;
+            }
+        }
+        void setMes(int m){mes=m;}
+        void setAnio(int a){anio=a;}
+        void setearFecha(int d, int m, int a){
+            dia=d;
+            mes=m;
+            anio=a;
+        }
+        int getDia(){return dia;}
+        int getMes(){return mes;}
+        int getAnio(){return anio;}
+        void Cargar();
+        void Mostrar(){
+            cout<<dia<<"/"<<mes<<"/"<<anio;
+        }
 };
 
 void Fecha::Cargar()
@@ -414,7 +393,7 @@ int ArchivoViajes::buscarRegistro(int num){
             pos++;
         }
         fclose(p);
-        return -2
+        return -2;
     }
 
 Viaje ArchivoViajes::leerRegistro(int pos){
